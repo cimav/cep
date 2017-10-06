952adca284c85f649c68a036dfd519930e3fa6f6
Geovany Gameros <geovanygameros@gmail.com>
2017-10-06 16:07:46 -0600

    Variables de clase en modelos de sapos

--- app/models/term_course_student.rb
+++ app/models/term_course_student.rb
@@ -2,4 +2,15 @@ class TermCourseStudent < RemoteModels
   belongs_to :term_course
   belongs_to :term_student
 
+  ACTIVE        = 1
+  INACTIVE      = 2
+  PENROLLMENT   = 6
+
+  STATUS = {ACTIVE   => 'Activo',
+            INACTIVE => 'Baja',
+            PENROLLMENT => 'Pre-inscrito'}
+
+  def status_type
+    STATUS[status]
+  end
 end
--- app/models/term_student.rb
+++ app/models/term_student.rb
@@ -1,4 +1,18 @@
 class TermStudent < RemoteModels
   has_many :term_course_student
   belongs_to :student
+
+  ACTIVE   = 1
+  PENDING  = 2
+  INACTIVE = 3
+  PENROLLMENT   = 6
+
+  STATUS = {ACTIVE   => 'Activo',
+            PENDING  => 'Con pendientes',
+            INACTIVE => 'Baja',
+            PENROLLMENT => 'Pre-inscrito'}
+
+  def status_type
+    STATUS[status]
+  end
 end
