aee946c73819550b1aad0f39cb35fe5297353a5a
Geovany Gameros <geovanygameros@gmail.com>
2017-10-31 14:07:54 -0700

    Se mejora el formato de las fechas, se cambia de sitio el uploader de documentos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,42 @@
 module ApplicationHelper
-  def active_class(link_path)
-     if request.fullpath == link_path
-       "active"
-     else
-       ""
-     end
+
+  def f_date(date)
+    if "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day}"
+      r_date = "Hoy"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != 0)
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day - 1}"
+      r_date = "Ayer"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != 0)
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day + 1}"
+      r_date = "Mañana"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != 0)
+    else
+      r_date = I18n.l(date, format: '%d de %B %Y a las %l:%M %P').capitalize
+      # d = "#{date.year}#{date.month}#{date.day}".to_i
+      # t = "#{Time.now.year}#{Time.now.month}#{Time.now.day}".to_i
+      # if d > t
+      #   r_date = "En #{t - d} días"
+      # else
+      #   r_date = "Hace #{d - t} días"
+      # end
+    end
+  end
+
+  def f_date_no_time(date)
+    if "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day}"
+      r_date = "Hoy"
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day - 1}"
+      r_date = "Ayer"
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day + 1}"
+      r_date = "Mañana"
+    else
+      d = "#{date.year}#{date.month}#{date.day}".to_i
+      t = "#{Time.now.year}#{Time.now.month}#{Time.now.day}".to_i
+      if d > t
+        r_date = "En #{d - t} días"
+      else
+        r_date = "Hace #{t - d} días"
+      end
+    end
   end
 end
