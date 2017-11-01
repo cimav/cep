d257ecfbfc24e464dc9ba37fab5496254bcaeb12
Geovany Gameros <geovanygameros@gmail.com>
2017-10-31 17:30:24 -0700

    Se elimina el uploader si no es admin

--- app/helpers/agreements_helper.rb
+++ app/helpers/agreements_helper.rb
@@ -2,7 +2,10 @@ module AgreementsHelper
 
   # Votos pendientes por usuario
   def missing_votes
-    @agreements = Agreement.where(status:Agreement::OPEN).where.not(id: Response.select(:agreement_id).where(user_id:current_user))
+    @agreements = Agreement.where(status:Agreement::OPEN).where.not(id: Response.select(:agreement_id).where(user_id:current_user.id))
   end
 
+  def last_agreement_votes
+    @last_agreement_votes = Agreement.where(id: Response.select(:agreement_id).where(user_id:current_user.id)).limit(10)
+  end
 end
